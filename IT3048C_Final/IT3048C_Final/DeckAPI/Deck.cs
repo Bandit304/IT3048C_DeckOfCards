c564e71f25dc6c588369ff3eec61f001e13d6fa9
Danny Murray <murraydp@mail.uc.edu>
2024-07-23 17:26:22 -0400

    Added folders and classes for MVVM structure

---
+++ IT3048C_Final/IT3048C_Final/DeckAPI/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IT3048C_Final.DeckAPI
+{
+    // Class to handle interactions with the Deck of Cards API
+    public class Deck
+    {
+        // Link to API
+        public string ApiUrl;
+        // ID for deck in API
+        public string DeckID;
+
+        public Deck(string apiUrl)
+        {
+            // Save Link to API
+            ApiUrl = apiUrl;
+            // Generate new Deck, save Deck ID
+        }
+    }
+}
---
+++ IT3048C_Final/IT3048C_Final/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using IT3048C_Final.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IT3048C_Final.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPageVM ViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ViewModel = new MainPageVM();
+        }
+    }
+}
